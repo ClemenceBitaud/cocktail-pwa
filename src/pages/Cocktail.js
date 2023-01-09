7b5f790cb66c7edd9364d77f3d619ed3cbb0647d
ClemenceBitaud <clemence.bitaud@gmail.com>
2023-01-10 00:42:40 +0100

    Feat : cocktail page without filtre OK

--- src/pages/Cocktail.js
+++ src/pages/Cocktail.js
@@ -3,6 +3,8 @@ import BottomNavBar from "../components/BottomNavBar";
 import {createStyles} from "@mantine/core";
 import useWindowDimensions from "../utils/windowDimensionHook";
 import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import CocktailsList from "../components/CocktailsList";
 
 const useStyles = createStyles((theme) => ({
 
@@ -31,10 +33,37 @@ const Cocktail = () => {
     const activeLink = "Cocktail";
     const {search} = useParams();
     console.log(search);
+    const [error, setError] = useState(null);
+    // const [setIsLoaded] = useState(false);
+    const [cocktails, setCocktails] = useState([]);
+
+    useEffect(() => {
+        if (search === undefined){
+            const saved = JSON.parse(localStorage.getItem("alcoholic-cocktails"));
+            setCocktails(saved);
+
+        }else{
+            fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`)
+                .then((res) => {
+                    return res.json();
+                })
+                .then((data) => {
+                    // setIsLoaded(true);
+                    const drinks = data.drinks;
+                    setCocktails(drinks);
+
+                }, (error) => {
+                    // setIsLoaded(true);
+                    setError(error);
+                });
+        }
+
+    }, [search]);
 
     return(
         <div className={classes.main}>
             <Header activeLink={activeLink}/>
+            <CocktailsList cocktails={cocktails} error={error}/>
             {width <= 600 ? <BottomNavBar activeLink={activeLink}/> : null}
         </div>
     )
